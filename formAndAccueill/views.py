61ea312af329b73164d1f771a9a3605685675185
MedLemineMbedah <med.lemine.mbedah@gmail.com>
2022-03-24 12:52:51 +0000

    add to git

---
+++ formAndAccueill/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def formAr_view(request):
+    return render(request,'formAr.html')
+
+def formFr_view(request):
+    return render(request,'formFr.html')
+
+def formEn_view(request):
+    return render(request,'formEn.html')
+
+def home_view(request):
+    return render(request,'Ac.html')
---
+++ readme.md
@@ -0,0 +1,2 @@
+1- lance  py manage.py runserver
+2- go to : http://127.0.0.1:8000/
